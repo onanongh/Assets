e038f883d95f5717242e4ad09e4657e6e0dd1646
onanong <onanong070@gmail.com>
2022-01-30 12:23:36 +0700

    commit naja

---
+++ PlayControl.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayControl : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float speed;
+    private int score;
+    public Text scoreText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        score =0;
+        SetScoreText();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement*speed);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("item"));
+        {
+            other.gameObject.SetActive(false);
+            score = score+1;
+            SetScoreText();
+        }
+    }
+    void SetScoreText()
+    {
+        scoreText.text = "Score: " +score.ToString();
+    }
+}
